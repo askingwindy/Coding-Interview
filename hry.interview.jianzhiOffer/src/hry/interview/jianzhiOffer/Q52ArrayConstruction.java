88dc96a1c9669c651d6dd9fc209b37e108210bf8
hry <RY@10.108.65.80>
2015-03-28 20:41:44 +0800

    52题：待解决

---
+++ hry.interview.jianzhiOffer/src/hry/interview/jianzhiOffer/Q52ArrayConstruction.java
@@ -0,0 +1,19 @@
+package hry.interview.jianzhiOffer;
+
+/**
+ * 构建一个乘积数组
+ * @author RY
+ *
+ */
+public class Q52ArrayConstruction {
+	/**
+	 * @param A 输入的数组
+	 * @param B 需要得到的乘积数组
+	 */
+	public void multiply(int[] A, int[] B){
+		if(A==null || A.length == 0 || B==null || A.length != B.length){
+			return;
+		}
+		
+	}
+}
