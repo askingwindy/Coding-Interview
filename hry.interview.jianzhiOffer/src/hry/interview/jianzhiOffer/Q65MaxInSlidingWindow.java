9fd1dde7ce62f89f4422adb167ed0caa248904bc
hry <RY@10.108.65.80>
2015-03-31 13:23:15 +0800

    65题：值得复习，注意corner case

--- hry.interview.jianzhiOffer/src/hry/interview/jianzhiOffer/Q65MaxInSlidingWindow.java
+++ hry.interview.jianzhiOffer/src/hry/interview/jianzhiOffer/Q65MaxInSlidingWindow.java
@@ -3,12 +3,26 @@ package hry.interview.jianzhiOffer;
 import java.util.Deque;
 import java.util.LinkedList;
 
+/**
+ * 求滑动窗口最大值
+ * 关键点：双端队列，队列中存可能成为最大值的数的Index
+ * @author RY
+ *
+ */
 public class Q65MaxInSlidingWindow {
 	
+	/**
+	 * 注意corner case:
+	 * windowSize == 0 以及nums.length < windowSize
+	 * @param nums
+	 * @param windowSize
+	 * @return
+	 */
 	public Integer[] maxInWindows(Integer[] nums, int windowSize){
 		if(nums==null || nums.length==0){
 			return nums;
 		}
+		
 		if(windowSize == 0 || nums.length < windowSize){
 			return null;
 		}
@@ -34,7 +48,7 @@ public class Q65MaxInSlidingWindow {
 			}
 			queue.offerLast(i);
 		}
-		//最后一个值在循环外求得
+		//！！最后一个值在循环外求得
 		result[count] = nums[queue.peekFirst()];
 		return result;
 	}
