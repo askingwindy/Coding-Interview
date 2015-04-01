e39d4d1f2d12db23ec1389cebd8dc927396783fe
hry <RY@10.108.65.80>
2015-04-01 13:49:33 +0800

    ListNode数据结构

--- hry.interview.jianzhiOffer/src/hry/interview/jianzhiOffer/ListNode.java
+++ hry.interview.jianzhiOffer/src/hry/interview/jianzhiOffer/ListNode.java
@@ -3,8 +3,31 @@ package hry.interview.jianzhiOffer;
 public class ListNode {
 	int val;
 	ListNode next;
-	ListNode(int x){
+	public ListNode(int x){
 		this.val = x;
 		this.next = null;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "ListNode [val=" + val + "]";
+	}
+
+
+	public void connectListNode(ListNode node){
+		this.next = node;
+	}
+	
+	public static void printList(ListNode head){
+		if(head == null){
+			System.out.println( "null");
+			return;
+		}
+		while(head.next!= null){
+			System.out.print(head.val + "-->");
+			head = head.next;
+		}
+		System.out.println(head.val);
 	}
 }
