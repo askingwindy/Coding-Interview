08d621566924809edd1935dfff0b60dc5fa446bc
hry <RY@10.108.65.80>
2015-04-01 11:40:50 +0800

    10题，求二进制中1的个数

---
+++ hry.interview.jianzhiOffer/src/hry/interview/jianzhiOffer/test/Q10NumberOf1InBinary.java
@@ -0,0 +1,23 @@
+package hry.interview.jianzhiOffer.test;
+
+/**
+ * 求得二进制数字中1的个数
+ * 已通过leetcode验证：
+ * @author RY
+ *
+ */
+public class Q10NumberOf1InBinary {
+    /**
+     * 思路是：n&(n-1)会将最后边的1变为0
+     * @param n
+     * @return
+     */
+    public int hammingWeight(int n) {
+        int count = 0;
+        while(n !=0){
+            count +=1;
+            n = n&(n-1);
+        }
+        return count;
+    }
+}
